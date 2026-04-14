f7c3b3a8ae1bfdc20f4393e6533a63aae669658c
Gustavo Pires Pinhal <gustavopirespinhal@gmail.com>
2026-04-14 09:33:16 -0400

    adiciona autenticação de usuário

--- src/Entity/Auth/Usuario.php
+++ src/Entity/Auth/Usuario.php
@@ -2,12 +2,23 @@
 
 namespace App\Entity\Auth;
 
-class Usuario
-{
-    private ?int $id = null;
-    private ?string $email = null;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Uid\Uuid;
 
-    public function getId(): ?int
+class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    private ?Uuid $id = null;
+    private ?string $email = null;
+    private array $roles = [];
+    private ?string $password = null;
+
+    public function __construct()
+    {
+        $this->roles = [];
+    }
+
+    public function getId(): ?Uuid
     {
         return $this->id;
     }
@@ -22,4 +33,35 @@ class Usuario
         $this->email = $email;
         return $this;
     }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function eraseCredentials(): void {}
 }
