581665707c14eb356d70f2dbea8fda0c5342db90
Gustavo Pires Pinhal <gustavopirespinhal@gmail.com>
2026-04-18 10:14:25 -0400

    atualiza chatMediaService

--- src/Service/ChatMediaService.php
+++ src/Service/ChatMediaService.php
@@ -2,7 +2,7 @@
 
 namespace App\Service;
 
-use App\Entity\Chat\Sala;
+use App\Entity\Chat\Arquivo;
 use Aws\S3\S3Client;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -15,32 +15,24 @@ class ChatMediaService
 
     public function uploadChatFile(
         UploadedFile $file,
-        Sala $sala,
+        Arquivo $arquivo,
     ): string {
         $mimeType = $file->getMimeType();
 
-        $typeDir = $this->determineDirectoryByMimeType($mimeType);
+        $idArquivo = $arquivo->getId();
+        $extencao = $file->guessExtension() ?? $file->getClientOriginalExtension();
 
-        $safeFilename = bin2hex(random_bytes(16));
-        $extension = $file->guessExtension() ?? $file->getClientOriginalExtension();
-
-        $fileName = sprintf(
-            'chats/%s/%s/%s.%s',
-            $sala->getId(),
-            $typeDir,
-            $safeFilename,
-            $extension
-        );
+        $nomeArquivo = "chats/{$idArquivo}.{$extencao}";
 
         $this->s3Client->putObject([
             'Bucket'              => $this->privateBucket,
-            'Key'                  => $fileName,
+            'Key'                  => $nomeArquivo,
             'Body'                 => fopen($file->getPathname(), 'r'),
             'ContentType'          => $mimeType,
             'ServerSideEncryption' => 'AES256',
         ]);
 
-        return $fileName;
+        return $nomeArquivo;
     }
 
     public function generateSecureUrl(string $filePath, string $expiration = '+20 minutes'): string
@@ -53,15 +45,5 @@ class ChatMediaService
         $request = $this->s3Client->createPresignedRequest($cmd, $expiration);
 
         return (string) $request->getUri();
-    }
-
-    private function determineDirectoryByMimeType(string $mimeType): string
-    {
-        return match (true) {
-            str_contains($mimeType, 'image') => 'imagens',
-            str_contains($mimeType, 'video') => 'videos',
-            str_contains($mimeType, 'audio') => 'audios',
-            default                          => 'others',
-        };
     }
 }
