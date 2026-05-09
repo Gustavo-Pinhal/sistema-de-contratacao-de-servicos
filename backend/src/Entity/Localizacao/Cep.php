5c3bfb3ec568a3633bea1b285f4172317da9badc
Gustavo Pires Pinhal <gustavopirespinhal@gmail.com>
2026-05-09 17:29:15 -0400

    Adiciona entidades Cep e Município

---
+++ backend/src/Entity/Localizacao/Cep.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity\Localizacao;
+
+class Cep
+{
+    private ?string $numero = null;
+    private ?string $rua = null;
+    private ?string $bairro = null;
+    private ?Municipio $municipio = null;
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): self
+    {
+        $this->numero = $numero;
+        return $this;
+    }
+
+    public function getRua(): ?string
+    {
+        return $this->rua;
+    }
+
+    public function setRua(string $rua): self
+    {
+        $this->rua = $rua;
+        return $this;
+    }
+
+    public function getBairro(): ?string
+    {
+        return $this->bairro;
+    }
+
+    public function setBairro(string $bairro): self
+    {
+        $this->bairro = $bairro;
+        return $this;
+    }
+
+    public function getMunicipio(): ?Municipio
+    {
+        return $this->municipio;
+    }
+
+    public function setMunicipio(?Municipio $municipio): self
+    {
+        $this->municipio = $municipio;
+        return $this;
+    }
+}
---
+++ backend/src/Entity/Localizacao/Municipio.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity\Localizacao;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+class Municipio
+{
+    private ?int $id = null;
+    private ?string $codigoIbge = null;
+    private ?string $nome = null;
+    private ?string $uf = null;
+
+    /** @var Collection<int, Cep> */
+    private Collection $ceps;
+
+    public function __construct()
+    {
+        $this->ceps = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodigoIbge(): ?string
+    {
+        return $this->codigoIbge;
+    }
+
+    public function setCodigoIbge(string $codigoIbge): self
+    {
+        $this->codigoIbge = $codigoIbge;
+        return $this;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getUf(): ?string
+    {
+        return $this->uf;
+    }
+
+    public function setUf(string $uf): self
+    {
+        $this->uf = $uf;
+        return $this;
+    }
+
+    /** @return Collection<int, Cep> */
+    public function getCeps(): Collection
+    {
+        return $this->ceps;
+    }
+}
