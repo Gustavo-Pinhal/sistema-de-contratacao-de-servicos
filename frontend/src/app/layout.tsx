dc90dd19d08f6910abfd48c0106698e5a2358910
Gustavo Pires Pinhal <gustavopirespinhal@gmail.com>
2026-05-13 00:48:04 -0400

    login funciona!

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,14 +4,16 @@ import "./globals.css";
 import { Providers } from "./providers";
 import { MainLayout } from "./components/MainLayout";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-br" className="h-full">
       <body className="h-full">
         <Providers>
-          <MainLayout>
-            {children}
-          </MainLayout>
+          <MainLayout>{children}</MainLayout>
         </Providers>
       </body>
     </html>
