5bd78b30cc7fa9d5d0bc24d9f9867090b3808bd0
Gustavo Pires Pinhal <gustavopirespinhal@gmail.com>
2026-05-14 21:48:01 -0400

    refatora Perfil

--- backend/src/Entity/Auth/Perfil.php
+++ backend/src/Entity/Auth/Perfil.php
@@ -4,34 +4,63 @@ namespace App\Entity\Auth;
 
 class Perfil
 {
-    private ?Usuario $usuario = null;
+    private Usuario $usuario;
+    private string $caminho;
+    private string $mimeType;
+    private int $tamanho;
 
-    private ?string $caminhoFoto = null;
-
-    public function __construct(Usuario $usuario, string $caminhoFoto)
-    {
+    public function __construct(
+        Usuario $usuario,
+        string $caminho,
+        string $mimeType,
+        int $tamanho
+    ) {
         $this->usuario = $usuario;
-        $this->caminhoFoto = $caminhoFoto;
+        $this->caminho = $caminho;
+        $this->mimeType = $mimeType;
+        $this->tamanho = $tamanho;
     }
 
     public function getId(): ?string
     {
-        return $this->usuario->getId();
+        return $this->usuario->getId()?->toString();
     }
 
-    public function getUsuario(): ?Usuario
+    public function getUsuario(): Usuario
     {
         return $this->usuario;
     }
 
-    public function getCaminhoFoto(): ?string
+    public function getCaminho(): string
     {
-        return $this->caminhoFoto;
+        return $this->caminho;
     }
 
-    public function setCaminhoFoto(string $caminhoFoto): self
+    public function setCaminho(string $caminho): self
     {
-        $this->caminhoFoto = $caminhoFoto;
+        $this->caminho = $caminho;
+        return $this;
+    }
+
+    public function getMimeType(): string
+    {
+        return $this->mimeType;
+    }
+
+    public function setMimeType(string $mimeType): self
+    {
+        $this->mimeType = $mimeType;
+        return $this;
+    }
+
+    public function getTamanho(): int
+    {
+        return $this->tamanho;
+    }
+
+    public function setTamanho(int $tamanho): self
+    {
+        $this->tamanho = $tamanho;
         return $this;
     }
 }
